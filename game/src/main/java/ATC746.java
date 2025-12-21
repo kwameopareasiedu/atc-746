2a301ab6d59dadddfbff1c4dff267c5e7c8d3788
Kwame Opare Asiedu <kwame@opare.dev>
2025-12-21 17:26:23 +0000

    Initialized repository. Initialized Maven repository with gamekit dependencies

---
+++ game/src/main/java/ATC746.java
@@ -0,0 +1,21 @@
+import dev.gamekit.core.Application;
+import dev.gamekit.settings.*;
+
+public class ATC746 extends Application {
+  public ATC746() {
+    super(
+      new Settings(
+        "ATC 746",
+        Resolution.FULL_HD,
+        Antialiasing.ON,
+        TextAntialiasing.ON,
+        RenderingStrategy.QUALITY
+      )
+    );
+  }
+
+  public static void main(String[] args) {
+    final ATC746 game = new ATC746();
+    game.run();
+  }
+}
